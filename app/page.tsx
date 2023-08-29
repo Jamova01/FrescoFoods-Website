4574ef4afc2fd35d1f8cb48e44951e1298c5645f
Jamova01 <jorgemova01@gmail.com>
2023-08-28 20:59:12 -0500

    Project configuration

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-green-500">
+        Hello, Next.js!
+      </h1>
+    </div>
+  );
+}
