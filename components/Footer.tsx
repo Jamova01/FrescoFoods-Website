59efa35575f85a5bc08ade6ad86c585f9053eba6
Jamova01 <jorgemova01@gmail.com>
2023-08-31 07:34:57 -0500

    [UPDATE]: Designing footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,20 @@
 export const Footer = () => {
-    return <footer><h2>Footer</h2></footer>
-}
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer
+      className="bg-green-500 p-4 text-white text-center font-semibold"
+      style={{
+        backgroundImage: 'url("/logo_v2.svg")',
+        backgroundRepeat: "repeat-x",
+        backgroundSize: "auto",
+        backgroundPosition: "center",
+      }}
+    >
+      <p>
+        © {currentYear} Fresco Foods Wholesale Grocers, S. L. Todos los derechos
+        reservados.
+      </p>
+    </footer>
+  );
+};
