5586a23bad55838a817488827beaf00e0edfec45
Jamova01 <jorgemova01@gmail.com>
2023-08-31 07:35:58 -0500

    [UPDATE: Implementing logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,24 +6,31 @@ export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="flex relative h-14 w-full">
+    <nav className="flex relative w-full bg-slate-100 shadow-lg">
       <button onClick={() => setToggle(!toggle)} className="p-4 absolute">
         {toggle ? (
-          <XMarkIcon className="h-6 w-6 text-black" />
+          <XMarkIcon className="w-7 h-7 text-black" />
         ) : (
-          <Bars3Icon className="h-6 w-6 text-black" />
+          <Bars3Icon className="w-7 h-7 text-black" />
         )}
       </button>
       <div className="m-auto">
-        <h1 className="text-2xl font-semibold">Logo</h1>
+        <span
+          className="w-40 h-14 inline-block"
+          style={{
+            backgroundImage: 'url("/logo.svg")',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></span>
       </div>
 
       {toggle && (
-        <div className="flex flex-col justify-center items-center absolute w-full h-[50vh] top-14 transition bg-slate-400 shadow-lg">
+        <div className="flex flex-col justify-center items-center absolute w-full h-[50vh] top-16 transition bg-slate-100 shadow-lg">
           <ul className="uppercase text-center text-xl w-full">
-            <li className="block p-3">home</li>
-            <li className="block p-3">products</li>
-            <li className="block p-3">guide</li>
+            <li className="block p-3 hover:text-orange-600 font-normal shadow-sm">home</li>
+            <li className="block p-3 hover:text-orange-600 font-normal shadow-sm">products</li>
+            <li className="block p-3 hover:text-orange-600 font-normal shadow-sm">guide</li>
           </ul>
         </div>
       )}
