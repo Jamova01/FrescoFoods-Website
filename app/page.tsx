953c7c3517a4acd4bd785b613a0a773bb5b16e4e
Jamova01 <jorgemova01@gmail.com>
2023-09-14 15:31:41 -0500

    [UPDATE]: Flyer august - september

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Page() {
               <Image src={flyer} alt="" objectFit="contain" layout="fill" />
             </div>
             <a
-              href="#"
+              href="public/FlyerAugust-September2023_FrescoFoods_compressed.pdf"
               download
               className="mt-2 inline-flex items-center h-9 px-4 bg-[#A8D42E] text-white font-semibold rounded-md"
             >
