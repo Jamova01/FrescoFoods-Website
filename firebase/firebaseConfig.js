850332394b7cbccb30b3d0246f7cc192bfff8c88
Jamova01 <jorgemova01@gmail.com>
2023-09-15 10:55:40 -0500

    [UPDATE]: set environment variables

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBukKtBVvYPvtYvc7HoE2tYlom6B9J950",
-  authDomain: "frescofoods-8d009.firebaseapp.com",
-  projectId: "frescofoods-8d009",
-  storageBucket: "frescofoods-8d009.appspot.com",
-  messagingSenderId: "865365113522",
-  appId: "1:865365113522:web:246d1f56156e9e75f627ab"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
