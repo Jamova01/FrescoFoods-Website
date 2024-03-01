9b547e27d317baea4f672e9f5a99980cb504107e
Jamova01 <jorgemova01@gmail.com>
2024-03-01 18:06:09 -0500

    Updating banners

--- app/page.tsx
+++ app/page.tsx
@@ -25,11 +25,10 @@ import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 
 const images = [
-  "https://res.cloudinary.com/dppyvfxef/image/upload/v1701919849/Fresco_Foods/banners/autumn-banners/image_2.png",
-  "https://res.cloudinary.com/dppyvfxef/image/upload/v1701919853/Fresco_Foods/banners/autumn-banners/image_3.png",
+  "https://res.cloudinary.com/dppyvfxef/image/upload/v1701919849/Fresco_Foods/banners/autumn-banners/image_1.png",
+  "https://res.cloudinary.com/dppyvfxef/image/upload/v1701919853/Fresco_Foods/banners/autumn-banners/image_2.png",
+  "https://res.cloudinary.com/dppyvfxef/image/upload/v1701919854/Fresco_Foods/banners/autumn-banners/image_3.png",
   "https://res.cloudinary.com/dppyvfxef/image/upload/v1701919854/Fresco_Foods/banners/autumn-banners/image_4.png",
-  "https://res.cloudinary.com/dppyvfxef/image/upload/v1701919854/Fresco_Foods/banners/autumn-banners/image_5.png",
-  "https://res.cloudinary.com/dppyvfxef/image/upload/v1707409233/Fresco_Foods/banners/autumn-banners/image_6.png",
   "https://res.cloudinary.com/dppyvfxef/image/upload/v1694815709/Fresco_Foods/banners/image_3.png",
   "https://res.cloudinary.com/dppyvfxef/image/upload/v1694815662/Fresco_Foods/banners/image_2.png",
 ];
