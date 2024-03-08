aa6cbb0e5e51be354cab400403bd7ca2a47ed15a
Jamova01 <jorgemova01@gmail.com>
2024-03-08 06:18:43 -0500

    Removing Fresco foods background

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,7 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer
-      className="bg-[#A8D42E] p-4 text-white text-center font-semibold"
-      style={{
-        backgroundImage: 'url("/logo_v2.svg")',
-        backgroundRepeat: "repeat-x",
-        backgroundSize: "auto",
-        backgroundPosition: "center",
-      }}
-    >
+    <footer className="bg-[#A8D42E] p-4 text-white text-center font-semibold">
       <p>
         © {currentYear} Fresco Foods Wholesale Grocers, S. L. Todos los derechos
         reservados.
